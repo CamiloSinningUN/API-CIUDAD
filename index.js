c78859742f105b8cda27c0f436510089b36dc978
CamiloSinningUN <camilosinning.cs@gmail.com>
2022-11-22 21:55:53 -0500

    for tutto

--- index.js
+++ index.js
@@ -3,29 +3,37 @@ const app = express();
 const port = process.env.PORT || 8080;
 const cors = require('cors');
 const morgan = require('morgan');
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const results = [];
+const cityToDepartment = new Map();
+
+fs.createReadStream('Departamentos_y_municipios_de_Colombia.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+        results.forEach((row) => {
+            // put everything in lowercase and remove accents
+            const city = row['MUNICIPIO'].normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+            const department = row['DEPARTAMENTO'].normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+            cityToDepartment.set(city, department);
+        });
+    });
+
+
 
 app.use(cors());
 app.use(morgan('dev'));
 
 app.get('/:city', (req, res) => {
     const city = req.params.city;
-    switch (city) {
-        case 'Bogota':
-            res.send('Cundinamarca');
-            break;
-        case 'Cartagena':
-            res.send('Bolivar');
-            break;
-        case 'Barranquilla':
-            res.send('Atlantico');
-            break;
-        case 'Santa Marta':
-            res.send('Magdalena');
-            break;
-        default:
-            res.send(city);
-            break;
-    }
+    // convert city to lower case and remove accents
+    const cityWithoutAccents = city.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+    const department = cityToDepartment.get(cityWithoutAccents);
+    // send response
+    res.send(department);
+
 });
 
 app.get('/', (req, res) => {
